f304ce98ebaab41e4b9d50a072c260f47aedcd11
1999karthikganesh <1999karthikganesh@gmail.com>
2024-02-28 20:06:47 +0530

    added initial commit

---
+++ src/main/java/com/democompany/mysecondproject/App.java
@@ -0,0 +1,13 @@
+package com.democompany.mysecondproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
